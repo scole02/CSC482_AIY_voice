4c080d587b6d94eda6ea4609dd3eb02946ff3244
Emily Gavrilenko <egavrile@calpoly.edu>
2021-12-05 17:23:47 -0800

    Add help message

--- query_builder.py
+++ query_builder.py
@@ -27,6 +27,11 @@ df_profs['last_name'] = df_profs['last_name'].str.lower()
 df_sched = df_sched.drop(columns=['Unnamed: 0'])
 df_sched = df_sched.drop_duplicates(keep='last')
 
+help_msg =  "Say Professor lastname " + \
+            "Ask about course name, description, section, time, instructor, location, enrollment capacity, " + \
+            "students enrolled, seats available for any quarter. " + \
+            "Ask about a professor's office, phone number, email, or alias."
+
 def complex_query_builder(input):
   """
   Create a complex pandas query from a combination of query parameters
@@ -392,6 +397,8 @@ def generate_response(df_name, query, col=[]):
       return "Ho Ho Ho, Merry Christmas!"
     if query == 2:
       return "Nothing"
+    if query == "Help":
+      return help_msg
     else:
       return "Not understood"
 
